b9764c87df05dcd4f0e340419bd7ac40c78b26bd
Edoardo Balducci <81587335+Bbalduzz@users.noreply.github.com>
2023-09-18 18:42:06 +0200

    Update sbkDL.py
    - added Book dataclass to have a tidier code
    - small improvement in retrieving book's datas

--- sbkDL.py
+++ sbkDL.py
@@ -1,11 +1,20 @@
 import requests, json, re
-from bs4 import BeautifulSoup
 import fitz
+from dataclasses import dataclass
 
 raw_url = input('Enter the book url: \n')
 
 with open('cookies.txt', 'r') as f: auth = f.readline()
 HEADERS = {'Cookie':auth}
+
+@dataclass
+class Book:
+	title:str
+	author:str
+	isbn:str
+	pages:str
+	toc: list[list]
+	identifier: str
 
 def convert_toc(toc_list):
     toc_dict = {}
@@ -24,22 +33,19 @@ def convert_toc(toc_list):
     return result
 
 def book_info():
-	req = requests.get(raw_url, headers=HEADERS).content
-	soup = BeautifulSoup(req, 'html.parser')
-	scripts = soup.select('script')[5].text
-	book_metadata = re.findall('(?<=books:)(.*)(?=)',scripts)[0].removesuffix(',')
+	req = requests.get(raw_url, headers=HEADERS).text
+	book_metadata = re.findall('(?<=books:)(.*)(?=)', req)[0].removesuffix(',')
 	book_metadata = json.loads(book_metadata)
 	raw_toc = book_metadata[0]['spine']['sections']
 	toc = convert_toc(raw_toc)
-	return book_metadata[0]['ws_title'], book_metadata[0]['ws_author'], book_metadata[0]['ws_isbn'], book_metadata[0]['ws_num_pages'], toc, book_metadata[0]['ws_book_id']
+	return Book(book_metadata[0]['ws_title'], book_metadata[0]['ws_author'], book_metadata[0]['ws_isbn'], book_metadata[0]['ws_num_pages'], toc, book_metadata[0]['ws_book_id'])
 
 def progress_bar(progress, total):
     percent = 100 * (progress / float(total))
     bar = '█' * int(percent) + '-' * (100 - int(percent))
     print(f"\r[+] Downloading book... |{bar}| {percent:.2f}% {progress}/{total}", end="\r")
 
-book_infos = book_info()
-book_id = book_infos[5]
+book = book_info()
 pages_per_req = [500]
 def div(n):
 	r = n-500
@@ -49,7 +55,7 @@ def div(n):
 
 def downloadbook():
 	doc = fitz.Document()
-	page_number = int(book_infos[3])
+	page_number = int(book.pages)
 	pages = []
 	if page_number > 500: # 414 Request-URI Too Large
 		div(page_number)
@@ -57,27 +63,27 @@ def downloadbook():
 			prev_elem = pages_per_req[n-1]
 			if prev_elem == pages_per_req[-1]: prev_elem = 1
 			attachment = [f'pages[]={n}&' for n in range(prev_elem, number_of_pages+prev_elem)]
-			pages_req = requests.get(f'https://webapp.scuolabook.it/books/{book_id}/pages?{"".join(attachment)}', headers=HEADERS).json()['pages'].values()
+			pages_req = requests.get(f'https://webapp.scuolabook.it/books/{book.identifier}/pages?{"".join(attachment)}', headers=HEADERS).json()['pages'].values()
 			pages.extend(list(pages_req))
 	else:
-		attachment = [f'pages[]={n}&' for n in range(1, int(book_infos[3]))] 
-		pages_req = requests.get(f'https://webapp.scuolabook.it/books/{book_id}/pages?{"".join(attachment)}', headers=HEADERS).json()['pages'].values()
+		attachment = [f'pages[]={n}&' for n in range(1, int(book.pages))] 
+		pages_req = requests.get(f'https://webapp.scuolabook.it/books/{book.identifier}/pages?{"".join(attachment)}', headers=HEADERS).json()['pages'].values()
 		pages.extend(list(pages_req))
-	progress_bar(0, book_infos[3])
+	progress_bar(0, book.pages)
 	for n,page in enumerate(pages):
 		page_data = requests.get(page, headers=HEADERS).content
 		page_doc = fitz.open(stream=page_data, filetype="jpg")
 		pdfbytes = page_doc.convert_to_pdf()
 		doc.insert_pdf(fitz.open("pdf",pdfbytes))
-		progress_bar(n, book_infos[3])
-	doc.set_toc(book_infos[4])
-	doc.save(f'{book_infos[0]}.pdf')
+		progress_bar(n, book.pages)
+	doc.set_toc(book.pages)
+	doc.save(f'{book.title}.pdf')
 
 print(f'''
 [+] Book Found:
-	- title: {book_infos[0]}
-	- author: {book_infos[1]}
-	- isbn: {book_infos[2]}
-	- pages: {book_infos[3]}
+	- title: {book.title}
+	- author: {book.author}
+	- isbn: {book.isbn}
+	- pages: {book.pages}
 ''')
 downloadbook()
